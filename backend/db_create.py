1cb440523b3d2ccbcd877e257001763556feb4af
vinay rayini <rayiniv@hotmail.com>
2017-03-18 21:33:44 -0500

    Added Playlist model and added relationships for video/category and video/playlist

--- backend/db_create.py
+++ backend/db_create.py
@@ -7,6 +7,16 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+video_category_table = Table('video_category', Base.metadata,
+    Column('categories_id', Integer, ForeignKey('category.id')),
+    Column('videos_id', Integer, ForeignKey('video.id'))
+)
+
+video_playlist_table = Table('video_playlist', Base.metadata,
+    Column('playlists_id', Integer, ForeignKey('playlist.id')),
+    Column('videos_id', Integer, ForeignKey('video.id'))
+)
+
 class Video(Base):
 	__tablename__ = "video"
 	id = Column(Integer, primary_key = True)
@@ -15,6 +25,11 @@ class Video(Base):
 	description = Column(String(250), nullable = False)
 	thumbnail = Column(String(250), nullable = False)
 	tags = Column(String(250), nullable = False)
+
+	channel_id = Column(Integer, ForeignKey('channel.id'))
+	channel = relationship("Channel", back_populates="videos")
+
+	categories = relationship("Category", secondary = video_category_table, back_populates = "videos")
 
 class Channel(Base):
 	__tablename__ = "channel"
@@ -34,4 +49,22 @@ class Category(Base):
 	title = Column(String(250), nullable = False)
 	latest_published_date = Column(DateTime, default = None)
 	num_videos = Column(Integer, nullable = False)
-	assignable = Column(Boolean, unique = False, default = True)		
+	assignable = Column(Boolean, unique = False, default = True)
+
+class Playlist(Base):
+	__tablename__ = "playlist"
+	id = Column(Integer, primary_key = True)
+
+	title = Column(String(250), nullable = False)
+	description = Column(String(250), nullable = False)
+	tags = Column(String(250), nullable = False)
+	published_date = Column(DateTime, default = None)
+	num_items = Column(Integer, default = None)
+
+	channel_id = Column(Integer, ForeignKey('channel.id'))
+	channel = relationship("Channel", back_populates="playlists")
+
+	videos = relationship("Video", secondary = video_playlist_table)
+
+engine = create_engine('sqlite:///sqlalchemy_example.db')
+Base.metadata.create_all(engine)
