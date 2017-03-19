1f74aeb724d0b06f662df3a1d09b1b3ae8228961
hamzamuhammad <hamzamuhammad@utexas.edu>
2017-03-18 21:30:28 -0500

    added channel and category models.

--- backend/db_create.py
+++ backend/db_create.py
@@ -15,3 +15,23 @@ class Video(Base):
 	description = Column(String(250), nullable = False)
 	thumbnail = Column(String(250), nullable = False)
 	tags = Column(String(250), nullable = False)
+
+class Channel(Base):
+	__tablename__ = "channel"
+	id = Column(Integer, primary_key = True)
+
+	title = Column(String(250), nullable = False)
+	description = Column(String(250), nullable = False)	
+	published_date = Column(DateTime, default = None)
+	country = Column(String(250), nullable = False)
+	view_count = Column(BigInteger, nullable = False)
+	subscriber_count = Column(BigInteger, nullable = False)
+
+class Category(Base):
+	__tablename__ = "category"
+	id = Column(Integer, primary_key = True)
+
+	title = Column(String(250), nullable = False)
+	latest_published_date = Column(DateTime, default = None)
+	num_videos = Column(Integer, nullable = False)
+	assignable = Column(Boolean, unique = False, default = True)		
