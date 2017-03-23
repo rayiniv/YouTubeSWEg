c6f870b9139b6751ea2d28d40d05029eba578cef
Voulgaropoulos, Nicole <nvoulgaropoulos@consultant.underarmour.com>
2017-03-22 22:10:15 -0500

    active navbar

---
+++ frontend/js/script.js
@@ -0,0 +1,32 @@
+document.getElementById("navMenu").innerHTML =
+'<nav id="navbarsweg">' +
+  '<nav class="navbar navbar-default">' +
+    '<div class="container-fluid">' +
+      '<!-- Brand and toggle get grouped for better mobile display -->' +
+      '<div class="navbar-header">' +
+        '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' +
+          '<span class="sr-only">Toggle navigation</span>' +
+          '<span class="icon-bar"></span>' +
+          '<span class="icon-bar"></span>' +
+          '<span class="icon-bar"></span>' +
+        '</button>' +
+        '<a class="navbar-brand" href="splash_page.html">YouTubeSWEg</a>' +
+      '</div>' +
+
+      '<!-- Collect the nav links, forms, and other content for toggling -->' +
+      '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+        '<ul class="nav navbar-nav">' +
+          '<li class=active" id="video"><a href="video.html">Videos</a></li>' +
+          '<li id="channel"><a href="channel.html">Channels</a></li>' +
+          '<li id="category"><a href="/category">Categories</a></li>' +
+          '<li id="playlist"><a href="/playlist">Playlists</a></li>' +
+          '<li id="about"><a href="#">About</a></li>' +
+
+        '</ul>' +
+        '<ul class="nav navbar-nav navbar-right">' +
+        '</ul>' +
+      '</div><!-- /.navbar-collapse -->' +
+    '</div><!-- /.container-fluid -->' +
+  '</nav>' +
+'</nav>';
+
